bf91ec361f3972f2f9923ff0df5fec4f596eea2c
Seab <honeyboyseab@gmail.com>
2017-01-09 13:51:25 -0500

    added tap gesture recognizer to enable dismissal of the keyboard by tapping the background view

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ConversionViewController: UIViewController {
     
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
     
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
         if let text = textField.text, !text.isEmpty {
@@ -18,7 +19,11 @@ class ConversionViewController: UIViewController {
         } else {
             celsiusLabel.text = "???"
         }
-       
     }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
     
 }
